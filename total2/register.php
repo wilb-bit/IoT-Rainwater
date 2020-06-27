ff02d494667acd192df4962943a2270c1f1a0cd6
wilb-bit <67494938+wilb-bit@users.noreply.github.com>
2020-06-27 15:21:31 +1000

    Add files via upload

---
+++ total2/register.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<form action="create_user.php" method="post">
+    Username: <input type="text" name="username"><br>
+    Password: <input type="text" name="password"><br>
+    First name: <input type="text" name="firstname"><br>
+    Last name: <input type="text" name="lastname"><br>
+    Email: <input type="text" name="email"><br>
+
+    <input type="submit">
+</form>
+
+</body>
+</html>
